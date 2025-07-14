cac56e6139c468936287acec6f3baa86fb723083
Jeff Shumate <jeff@catapulsion.net>
2025-07-14 15:31:15 -0600

    add location history provider

---
+++ LocationTrackerDemo/LocationTrackerDemo/LocationTrackerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTrackerDemoApp.swift
+//  LocationTrackerDemo
+//
+//  Created by Jeff Shumate on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationTrackerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationTrackerDemo/LocationTrackerDemoTests/LocationTrackerDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  LocationTrackerDemoTests.swift
+//  LocationTrackerDemoTests
+//
+//  Created by Jeff Shumate on 6/23/25.
+//
+
+import Testing
+
+struct LocationTrackerDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
